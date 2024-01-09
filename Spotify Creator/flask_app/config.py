d3b80d34d9e8d362f096a4b5963bea8ae9f5cae1
Jared Fleming <82134861+jaredFleming18@users.noreply.github.com>
2024-01-09 15:16:26 -0500

    Delete Spotify Creator/flask_app/config.py

--- Spotify Creator/flask_app/config.py
+++
@@ -1,3 +0,0 @@
-# Stores all configuration values
-SECRET_KEY = b'$2b$12$yy3gB0pcIk4tbkYGlerjnetCz.xGHTabtFmP/HSFyyW5B3rf6.1uG'
-MONGODB_HOST = 'mongodb+srv://jaredflemingo:jaredflemingo@cluster0.a4cznwl.mongodb.net/my_database?retryWrites=true&w=majority'
